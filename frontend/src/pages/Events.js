cf5638fab77be92e7336e8821eca5e5afccebe94
sunil parmar <sunil04ce@gmail.com>
2023-03-07 23:35:05 +0530

    use of loader to fetch data from api, useLoaderData

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,29 +1,13 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
-const DUMMY_EVENTS = [
-  {
-    id: "e1",
-    title: "Some event 1",
-  },
-  {
-    id: "e2",
-    title: "Another event 2",
-  },
-];
-
-const EventsPage = () => {
+function EventsPage() {
+  const events = useLoaderData();
   return (
     <>
-      <h1>EventsPage</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsList events={events} />
     </>
   );
-};
+}
 
 export default EventsPage;
