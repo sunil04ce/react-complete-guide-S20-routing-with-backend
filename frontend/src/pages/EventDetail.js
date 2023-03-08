7dbc5ba17c641210bc9bbb7dbc0d71d371735f52
sunil parmar <sunil04ce@gmail.com>
2023-03-08 17:48:51 +0530

    use of useSubmit, use of action to delete event

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,29 +1,48 @@
-import { useParams, useRouteLoaderData, json } from "react-router-dom";
+import { useRouteLoaderData, json, redirect } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 const EventDetailPage = () => {
-    // const params = useParams();
-    const data = useRouteLoaderData("event-detail");
+  // const params = useParams();
+  const data = useRouteLoaderData("event-detail");
 
-    return (
-        <>
-            {/* <h1>EventDetailPage</h1>
+  return (
+    <>
+      {/* <h1>EventDetailPage</h1>
             <p>Event Id : {params.eventId}</p> */}
-            <EventItem event={data.event} />
-        </>
-    )
+      <EventItem event={data.event} />
+    </>
+  );
 };
 
 export default EventDetailPage;
 
 export async function loader({ request, params }) {
-    const id = params.eventId;
+  const id = params.eventId;
 
-    const response = await fetch('http://localhost:8080/events/' + id);
+  const response = await fetch("http://localhost:8080/events/" + id);
 
-    if (!response.ok) {
-        throw json({ message: "Could not fetch details for selected event." }, { status: 500 });
-    }
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch details for selected event." },
+      { status: 500 }
+    );
+  }
 
-    return response;
-};
+  return response;
+}
+
+export async function action({ params, request }) {
+  const eventId = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + eventId, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not delete selected event." },
+      { status: 500 }
+    );
+  }
+
+  return redirect("/events");
+}
--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,36 +1,33 @@
-import { json, redirect } from 'react-router-dom';
-import EventForm from '../components/EventForm';
+import { json, redirect } from "react-router-dom";
+import EventForm from "../components/EventForm";
 
 const NewEventPage = () => {
-    return (
-        <EventForm> </EventForm>
-    )
+  return <EventForm> </EventForm>;
 };
 
 export default NewEventPage;
 
 export async function action({ request, params }) {
-    const data = await request.formData();
+  const data = await request.formData();
 
-    const eventData = {
-        title: data.get('title'),
-        image: data.get('image'),
-        date: data.get('date'),
-        description: data.get('description'),
-    };
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
 
-    const response = await fetch("http://localhost:8080/events", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(eventData),
-    });
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
 
-    if (!response.ok) {
-        throw json({ message: 'Could not save event.' }, { status: 500 });
-    }
+  if (!response.ok) {
+    throw json({ message: "Could not save event." }, { status: 500 });
+  }
 
-    return redirect('/events');
-
-};
+  return redirect("/events");
+}
